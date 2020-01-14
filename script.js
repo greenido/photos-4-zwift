2219c8307edc48c0c676db4b359002f365ae6679
Glitch (photos-4-zwift) <none>
2020-01-14 23:51:46 +0000

    👐 more comments

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-console.log("Start the party 🚴🏼‍♂️");
+//
+//
+// Some inspiration with photos on your next Zwift workout"
+// Author: @greenido
+// Date: Jan 2019
+//
+//
+console.log("* Start the party 🚴");
 
-// control the auto refresh rate -> 1000 = 1 second
+// The auto refresh rate (1000 = 1 second)
 var refreshRate = 10000; // 10 seconds
 
+// Check if we already have a pre-set value
 if (localStorage.getItem("refreshRate")) {
   refreshRate = localStorage.getItem("refreshRate");
 } else {
@@ -12,18 +20,16 @@ let refSelectVal = parseInt(refreshRate);
 $("#refresh-select").val(refSelectVal);
 
 //
-//
+// When the user change the refresh rate - store it and reload
 //
 $("#refresh-select").change(function() {
   refreshRate = $(this).val();
   localStorage.setItem("refreshRate", refreshRate);
   clearInterval(mainInterval);
-  setInterval(function() {
-    location.reload();
-  }, refreshRate);
+  location.reload();
 });
 
-// Automatically reload the browser
+// Automatically reload the page with a new image every 'refreshRate' time
 let mainInterval = setInterval(function() {
   location.reload();
 }, refreshRate);
@@ -33,10 +39,13 @@ $("#foot").hide();
 
 // Get a random image from Unsplash and fade it in on load
 $("#foot")
-  .css({"background-image": "url(https://source.unsplash.com/1600x900/?nature,water,snowboard,surf,ironman,cycling,running,mountains)"
-  }).fadeIn(1500);
+  .css({
+    "background-image":
+      "url(https://source.unsplash.com/1600x900/?nature,water,snowboard,surf,ironman,cycling,running,mountains)"
+  })
+  .fadeIn(1000);
 
-//
+// TODO: check if we wish to have this data on our BackEnd
 // $.get("https://www.cloudflare.com/cdn-cgi/trace", function(data) {
 //   console.log("🟣 We got: ");
 //   console.log(data);
